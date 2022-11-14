7ebf9f42146096d2ffc12bcf9de2428dd84a860e
Archaversine <adam.brohl.w@gmail.com>
2022-11-14 11:08:49 -0600

    Better Tape Visualization

--- TapeVisualizer.py
+++ TapeVisualizer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-def visualize(tape: list, selected_pos: int = -1, start: int = 0, cells: int = 0) -> None:
+def visualize_str(tape: list, selected_pos: int = -1, start: int = 0, cells: int = 0) -> str:
+
+    output = ''
 
     #iterations = lambda: range(start, len(tape[start:start+(cells or len(tape))]) + 1)
     upper = min(start + (cells or len(tape)) + 1, len(tape) + 1)
@@ -8,14 +10,14 @@ def visualize(tape: list, selected_pos: int = -1, start: int = 0, cells: int = 0
 
     for i in iterations():
         cell_header = f'{"▼" if i - 1 == selected_pos and i != 0 else "": ^5}'
-        print(f'{cell_header} ', end='')
+        output += f'{cell_header} '
 
-    print()
+    output += ' ' * 5 + '\n'
 
     for _ in iterations():
-        print('─────┬', end='')
+        output += '─────┬'
 
-    print('─────')
+    output += '─────\n'
 
     for i in iterations():
 
@@ -24,20 +26,24 @@ def visualize(tape: list, selected_pos: int = -1, start: int = 0, cells: int = 0
         if len(center) == 2:
             center = ' ' + center
 
-        print(f'{center : ^5}│', end='')
+        output += f'{center : ^5}│'
 
-    print(f'{("..." if upper <= len(tape) else "" ) : ^5}')
+    output += f'{("..." if upper <= len(tape) else "" ) : ^5}\n'
 
     for _ in iterations():
-        print('─────┴', end='')
+        output += '─────┴'
 
-    print('─────')
+    output += '─────\n'
 
     for i in iterations():
         cell_header = f'{"▲" if i - 1 == selected_pos and i != 0 else "": ^5}'
-        print(f'{cell_header} ', end='')
+        output += f'{cell_header} '
 
-    print()
+    output += ' ' * 5 + '\n'
+    return output
+
+def visualize(tape: list, selected_pos: int = -1, start: int = 0, cells: int = 0) -> None:
+    print(visualize_str(tape, selected_pos, start, cells))
 
 if __name__ == '__main__':
     visualize([1, 2, 38, 4, 255, 9, 8, 7, 6], selected_pos=5, start=2, cells=6)
